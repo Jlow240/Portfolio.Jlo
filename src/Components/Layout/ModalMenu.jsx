30dd4f3eb9c1a3326a44bd3786246b511de37414
Juan Luis Orozco <juanluis.ow@gmail.com>
2023-05-01 21:11:11 -0600

    Menu item animation

--- src/Components/Layout/ModalMenu.jsx
+++ src/Components/Layout/ModalMenu.jsx
@@ -7,15 +7,50 @@ const ModalMenu = ({ showNav, handleClickShowNavbar }) => {
 
 
         <section className={`mob-conteiner ${showNav}`}>
-            <ul className="mob-list">  
-            <div className='mob-close'>
-                <i onClick={handleClickShowNavbar} className='bx bx-x-circle'></i>
-            </div>
-                <li><a className="mob-list__item" href="#home"> Home</a></li>
-                <li><a className="mob-list__item" href="#aboutMe">About Me</a></li>
-                <li><a className="mob-list__item" href="#skills">Skills</a></li>
-                <li><a className="mob-list__item" href="#projects">Projects</a></li>
-                <li><a className="mob-list__item" href="#contact">Contact</a></li>
+            <ul className="mob-list">
+                <div className='mob-close'>
+                    <i onClick={handleClickShowNavbar} className='bx bx-x-circle'></i>
+                </div>
+                <li className="mob-list__item">
+                    <a href="#home">
+                        <span>Home</span>
+                        <span>
+                            <i class='bx bx-home-heart'></i>
+                        </span>
+                    </a>
+                </li>
+                <li className="mob-list__item">
+                    <a href="#aboutMe">
+                        <span>About Me</span>
+                        <span>
+                            <i class='bx bxs-user-badge'></i>
+                        </span>
+                    </a>
+                </li>
+                <li className="mob-list__item">
+                    <a href="#skills">
+                        <span>Skills</span>
+                        <span>
+                            <i class='bx bx-code-curly'></i>
+                        </span>
+                    </a>
+                </li>
+                <li className="mob-list__item">
+                    <a href="#projects">
+                        <span>Projects</span>
+                        <span>
+                            <i class='bx bxs-layout'></i>
+                        </span>
+                    </a>
+                </li>
+                <li className="mob-list__item">
+                    <a href="#contact">
+                        <span>Contact</span>
+                        <span>
+                            <i class='bx bxs-contact' ></i>
+                        </span>
+                    </a>
+                </li>
             </ul>
         </section>
     )
